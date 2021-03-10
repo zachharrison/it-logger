86cce8cb1d239932a5cf3231cd74179706877a5a
Zach Harrison <zharrison@shaw.ca>
2021-03-09 18:08:14 -0800

    Added Logs and LogItem components and now rendering data from db

---
+++ src/components/logs/Logs.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+
+const Logs = () => {
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getLogs();
+    // eslint-disable-next-line
+  }, []);
+
+  const getLogs = async () => {
+    setLoading();
+    const res = await fetch('/logs');
+    const data = await res.json();
+
+    setLogs(data);
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <h4>Loading...</h4>;
+  }
+
+  return (
+    <ul className='collection-width-header'>
+      <li className='collection-header'>
+        <h4 className='center'>System Logs</h4>
+      </li>
+      {!loading && logs.length === 0 ? (
+        <p className='center'>No logs to show</p>
+      ) : (
+        logs.map((log) => <li>{log.message}</li>)
+      )}
+    </ul>
+  );
+};
+
+export default Logs;
