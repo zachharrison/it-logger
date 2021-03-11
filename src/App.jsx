9aa459ac7f75fd87dfb7cb9005b3d91d59b49e68
Zach Harrison <zharrison@shaw.ca>
2021-03-10 17:47:06 -0800

    Added modal component for adding new technicians

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import SearchBar from './components/layout/SearchBar';
 import AddBtn from './components/layout/AddBtn';
 import Logs from './components/logs/Logs';
 import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -20,6 +22,8 @@ const App = () => {
       <div className='container'>
         <AddBtn />
         <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
         <Logs />
       </div>
     </Fragment>
