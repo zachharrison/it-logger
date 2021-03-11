7a92515dff03cd7b35bda1491a11abccf9e9c080
Zach Harrison <zharrison@shaw.ca>
2021-03-11 11:31:44 -0800

    Completed updatelog feature

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,13 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from './types';
+import { 
+  GET_LOGS, 
+  SET_LOADING, 
+  LOGS_ERROR, 
+  ADD_LOG, 
+  DELETE_LOG,
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT
+} from './types';
 
 export const setLoading = () => {
   return {
@@ -64,6 +73,49 @@ export const deleteLog = (id) => async (dispatch) => {
     dispatch({
       type: DELETE_LOG,
       payload: id
+    });
+
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    })
+  }
+};
+
+// SET CURRENT LOG
+export const setCurrent = (log) => {
+  return { 
+    type: SET_CURRENT, 
+    payload: log 
+  }
+}
+
+// CLEAR CURRENT LOG
+export const clearCurrent = () => {
+  return { 
+    type: CLEAR_CURRENT 
+  }
+}
+
+// UPDATE LOG ON SERVER
+export const updateLog = (log) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs/${log.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    const data = await res.json()
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data
     });
 
   } catch (error) {
